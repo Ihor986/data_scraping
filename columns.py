4f98871a9bed0fb4cc1b3f1c41a97567093342ed
Ihor986 <235689ks@gmail.com>
2024-07-13 00:45:27 +0300

    create restcountries class

---
+++ columns.py
@@ -0,0 +1,7 @@
+from abc import ABC
+
+class CountriesColumns(ABC):
+    COUNTRY_NAME = 'назва країни'
+    CAPITAL_NAME = 'назва столиці'
+    FLAG = 'прапор'
+    
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import requests
+from api_client import APIClient
+from restcountries import RestCountries
 
 def main():
-    ...
+    print(rest_countries.all_countries_df)
 
 
 if __name__ == "__main__":
+    api_client = APIClient()
+    rest_countries = RestCountries(
+        api_client=api_client, 
+        base_url='https://restcountries.com/v3.1'
+        )
+    
     main()
